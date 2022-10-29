d62d869b4857cc3191c28112c90141075b477202
chaaals <charlesching0808@gmail.com>
2022-10-29 21:38:05 +0800

    feat: add static version of quiz page

--- src/assets/images/gdsc-logo/GDSC-PLM.logo.jsx
+++ src/assets/images/gdsc-logo/GDSC-PLM.logo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const GDSCLogo = () => (
-  <img src={require("./gdsc-logo.png")} alt="gdsc-plm-logo" />
+  <img className="logo" src={require("./gdsc-logo.png")} alt="gdsc-plm-logo" />
 );
 
 export default GDSCLogo;
