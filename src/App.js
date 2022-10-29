b4a101e04c7bfd82e6389d086c0783bc4b05bbe3
chaaals <charlesching0808@gmail.com>
2022-10-29 13:30:56 +0800

    feat: add pre-loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React, { useState } from "react";
 function App() {
-  return <div>Hello World</div>;
+  const [loading, setLoading] = useState(true);
+  const preloader = document.getElementById("pre-loader");
+
+  if (preloader) {
+    setTimeout(() => {
+      preloader.style.display = "none";
+      setLoading(false);
+    }, 1500);
+  }
+  return !loading && <div>Hello World</div>;
 }
 
 export default App;
